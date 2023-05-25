921998c8e3a5eb76a697a7f48190b0c9911dc692
Hezkiaruan <hezkiaruan@gmail.com>
2023-05-25 20:50:00 +0700

    membuat web baju halimawan

---
+++ js/daftar.js
@@ -0,0 +1,10 @@
+const logregBox = document.querySelector('.logreg-box');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+
+registerlink.addEventListener('click', () => {
+    logregBox.classList.add('active');
+});
+loginlink.addEventListener('click', () => {
+    logregBox.classList.remove('active');
+});
